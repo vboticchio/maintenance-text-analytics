27d5dd146f4ce7d0fa865c6728976d6a5819c926
vboticchio <vboticchio@gmail.com>
2026-02-23 10:01:40 -0300

    Update vectorization.py

--- src/vectorization.py
+++ src/vectorization.py
@@ -1,1 +1,12 @@
+from sklearn.feature_extraction.text import TfidfVectorizer
 
+def create_vectorizer():
+    return TfidfVectorizer(
+        max_features=3000,
+        ngram_range=(1, 2)
+    )
+
+def vectorize_text(vectorizer, texts):
+    X = vectorizer.fit_transform(texts)
+    terms = vectorizer.get_feature_names_out()
+    return X, terms
