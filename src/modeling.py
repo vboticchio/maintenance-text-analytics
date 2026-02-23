af03dd3c7e6feb83052452d6a922f8d6fedb2562
vboticchio <vboticchio@gmail.com>
2026-02-23 10:01:52 -0300

    Update modeling.py

--- src/modeling.py
+++ src/modeling.py
@@ -1,1 +1,10 @@
+from sklearn.svm import LinearSVC
+from sklearn.model_selection import train_test_split
 
+def train_svm(X, y):
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=0.2, random_state=42
+    )
+    model = LinearSVC()
+    model.fit(X_train, y_train)
+    return model
